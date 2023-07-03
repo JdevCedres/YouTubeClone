d2c6f63809634f186cecc417cbc0126788437448
Jose Damaso gonzalez cedres <josedamasogonzalezcedres@iMac-de-Jose.home>
2023-07-03 17:27:45 +0100

    clase no 2

---
+++ YouTubeClone/YouTubeClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YouTubeClone
+//
+//  Created by Jose Damaso gonzalez cedres on 3/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
